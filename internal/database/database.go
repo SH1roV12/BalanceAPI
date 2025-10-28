d951b73ef3f343c98c2e88cb78b5c92be2dea642
Zakhar <yaharuott@localhost.localdomain>
2025-10-28 15:20:40 +0700

    add packet for getting config for open database

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,21 +3,13 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"os"
+	getconfig "users-balance/internal/getConfig"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
 func InitDB() *sql.DB {
-	godotenv.Load()
-	user := os.Getenv("DB_USER")
-	pass := os.Getenv("DB_PASS")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	name := os.Getenv("DB_NAME")
-
-	var DSN string = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pass, host, port, name)
+	DSN:=getconfig.GetConfig()
 
 	db, err := sql.Open("mysql", DSN)
 	if err != nil {
---
+++ internal/getConfig/getConfig.go
@@ -0,0 +1,22 @@
+package getconfig
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+
+
+func GetConfig()string{
+	godotenv.Load()
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	var DSN string = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pass, host, port, name)
+	return DSN
+}
