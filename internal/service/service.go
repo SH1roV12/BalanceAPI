0e00b26976ee30c49580dfd6dc8dafe03605afd7
Zakhar <yaharuott@localhost.localdomain>
2025-10-28 15:22:39 +0700

    'r' to 's' in service

--- internal/service/service.go
+++ internal/service/service.go
@@ -16,27 +16,27 @@ func NewService(repo *repository.Repository) *UsersService {
 	return &UsersService{Repo: repo}
 }
 
-func (r *UsersService) CreateUser(ctx context.Context, user *models.User) error {
-	return r.Repo.CreateUser(ctx, user)
+func (s *UsersService) CreateUser(ctx context.Context, user *models.User) error {
+	return s.Repo.CreateUser(ctx, user)
 }
 
-func (r *UsersService) GetUserBalance(ctx context.Context, usr dto.GetUserBalance) (float64, float64, error) {
-	return r.Repo.GetUserBalance(ctx, usr.UserId)
+func (s *UsersService) GetUserBalance(ctx context.Context, usr dto.GetUserBalance) (float64, float64, error) {
+	return s.Repo.GetUserBalance(ctx, usr.UserId)
 }
 
-func (r *UsersService) ReserveUserBalance(ctx context.Context, usr dto.ReserveUserBalance) (float64, float64, error) {
-	balance, reserved, err := r.Repo.ReserveUserBalance(ctx, usr.UserId, usr.Amount)
+func (s *UsersService) ReserveUserBalance(ctx context.Context, usr dto.ReserveUserBalance) (float64, float64, error) {
+	balance, reserved, err := s.Repo.ReserveUserBalance(ctx, usr.UserId, usr.Amount)
 	if err != nil {
 		return 0, 0, err
 	}
 	return balance, reserved, err
 }
 
-func (r *UsersService) ReplenishmentOfBalance(ctx context.Context, usr dto.ReplenishmentOfBalance) (float64, error) {
+func (s *UsersService) ReplenishmentOfBalance(ctx context.Context, usr dto.ReplenishmentOfBalance) (float64, error) {
 	if usr.Amount <= 0 {
 		return 0, errors.New("incorrect amount")
 	}
-	balance, error := r.Repo.ReplenishmentOfBalance(ctx, usr.UserID, usr.Amount)
+	balance, error := s.Repo.ReplenishmentOfBalance(ctx, usr.UserID, usr.Amount)
 	if error != nil {
 		return 0, error
 	}
